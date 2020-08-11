3177d00bbde979dac5cbbfb97d8d975d3b71d52f
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-08-11 17:28:16 +0530

    new: transfer data from matic to eth

--- script/exit.js
+++ script/exit.js
@@ -1,13 +1,13 @@
-const config = require("../config");
 const utils = require("../utils");
 const maticPOSClient = utils.getMaticPOSClient();
 
+const txHash = "tx hash from matic";
+
 const execute = async () => {
   try {
-    const tx = await maticPOSClient.depositERC721ForUser(
-      config.root.DERC721,
-      config.user.address,
-      config.user.tokenId
+    const tx = await maticPOSClient.posRootChainManager.exit(
+      txHash,
+      "0xb7e5ddf32ab01f4630c5cf14dac344aba9e35bc5d91d1aa31c55f13c19094913"
     );
     console.log(tx.transactionHash); // eslint-disable-line
   } catch (e) {
