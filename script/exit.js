be91ea8bc79ba2aea181420e2ba38f80c9fae2cf
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-08-11 21:26:54 +0530

    new: contracts

--- script/exit.js
+++ script/exit.js
@@ -1,13 +1,14 @@
 const utils = require("../utils");
 const maticPOSClient = utils.getMaticPOSClient();
 
-const txHash = "tx hash from matic";
+const txHash =
+  "0xc9f6ba0ad48a8812a0b22bcb8a205f3c5cb41ccf1ce2908b6ebe84a5e6c1a64f";
 
 const execute = async () => {
   try {
     const tx = await maticPOSClient.posRootChainManager.exit(
       txHash,
-      "0xb7e5ddf32ab01f4630c5cf14dac344aba9e35bc5d91d1aa31c55f13c19094913"
+      "0xd67e513db128e9551b5eea287725891be42d9cf2596c29bbbfecf012ba6799e4"
     );
     console.log(tx.transactionHash); // eslint-disable-line
   } catch (e) {
