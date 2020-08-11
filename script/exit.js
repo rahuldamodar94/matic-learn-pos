41c286c09c9b3cc58e7e2cdd2d4f5910be98af64
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-08-12 03:37:50 +0530

    chg: logeventsig

--- script/exit.js
+++ script/exit.js
@@ -4,11 +4,14 @@ const maticPOSClient = utils.getMaticPOSClient();
 const txHash =
   "0xc9f6ba0ad48a8812a0b22bcb8a205f3c5cb41ccf1ce2908b6ebe84a5e6c1a64f";
 
+const logEventSignature =
+  "0x46269ee522654e4067695b81b5bcafa7f76e1d31ef24c997420450b0c6626531";
+
 const execute = async () => {
   try {
     const tx = await maticPOSClient.posRootChainManager.exit(
       txHash,
-      "0xd67e513db128e9551b5eea287725891be42d9cf2596c29bbbfecf012ba6799e4"
+      logEventSignature
     );
     console.log(tx.transactionHash); // eslint-disable-line
   } catch (e) {
