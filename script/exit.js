44e3b134b0532805ce90ae2e57c185f80eabf453
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-08-15 23:34:15 +0530

    custome predicate

--- script/exit.js
+++ script/exit.js
@@ -2,7 +2,7 @@ const utils = require("../utils");
 const maticPOSClient = utils.getMaticPOSClient();
 
 const txHash =
-  "0xb4a942175121fd61594499d57b84bf8e50e034c171d4ff66b5920fd7df80027f";
+  "0xc094de3b7abd29f23a23549d9484e9c6bddb2542e2cc0aa605221cb55548951c";
 
 const logEventSignature =
   "0x93f3e547dcb3ce9c356bb293f12e44f70fc24105d675b782bd639333aab70df7";
