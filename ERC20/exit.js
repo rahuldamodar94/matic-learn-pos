a26f5f87e07cef75d4a35ea0ca1f87878be3d896
rahuldamodar94 <rahuldamodar94@gmail.com>
2020-12-24 08:46:58 +0530

    new api for payload pos exit

--- ERC20/exit.js
+++ ERC20/exit.js
@@ -1,19 +1,19 @@
 const utils = require("../utils");
 const maticPOSClient = utils.getMaticPOSClient();
 
-const burnHash =
-  "0xb511fa1620e65e3d1931ef3c955bc528d4c9e8640d78a17cace81636948d22fd";
-
-const exit = async () => {
+const exit = async (txHash) => {
   try {
-    const payload = await maticPOSClient.posRootChainManager.getExitPayload(burnHash)
+    const payload = await maticPOSClient.posRootChainManager.getExitPayload(txHash)
     return (payload)
   } catch (e) {
     console.error(e); // eslint-disable-line
   }
 };
 
-exit().then(res => {
+const burnHash =
+  "0xb511fa1620e65e3d1931ef3c955bc528d4c9e8640d78a17cace81636948d22fd";
+
+exit(burnHash).then(res => {
   console.log(res)
 }).catch(err  => {
   console.log(err)
